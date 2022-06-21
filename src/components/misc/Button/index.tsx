cbfc6e46a2da4f41c3bfeebb7d790be33f5cf143
Santhosh Siva <santhoshsiva@Santhoshs-MacBook-Pro.local>
2022-06-21 22:57:33 +0530

    Fixed buttons

--- src/components/misc/Button/index.tsx
+++ src/components/misc/Button/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.scss';
 
-const Button = ({ onClick = (id: string) => {}, id = '' }) => {
+const Button = ({ name = '', onClick = (id: string) => {}, id = '' }) => {
 	const handleClick = () => onClick(id);
 	return (
-		<button className="fret_revealer fret_revealer_last" onClick={handleClick}>
-			Toggle Positions
+		<button className="fret_revealer" onClick={handleClick}>
+			{name}
 		</button>
 	);
 };
--- src/components/misc/FretToolBar/index.tsx
+++ src/components/misc/FretToolBar/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Marker from '../Marker';
+import Button from '../Button';
 import './index.scss';
 
 const FretToolbar = ({
 	onClickMarker = (isBasic: boolean) => {},
+	onToggleNotes = () => {},
+	onToggleFrets = () => {},
 	activeMarker = 1
 }) => {
+
 	return (
 		<div className="fret-tool-bar">
 			<div className="markers">
@@ -16,6 +20,8 @@ const FretToolbar = ({
 					isBasic={false}
 				/>
 			</div>
+			<Button name="Toggle Note's" onClick={onToggleNotes}/>
+			<Button name="Toggle Fret Number's" onClick={onToggleFrets}/>
 		</div>
 	);
 };

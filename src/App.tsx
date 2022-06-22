eec04b36c8e142d9d1a99a672aa0ecbf8d7fca50
Santhosh Siva <santhoshsiva@Santhoshs-MacBook-Pro.local>
2022-06-22 12:24:50 +0530

    fixed social link

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
 		<div className="App">
 			<Home />
 			<div className="footer">
-				<a href="https://www.linkedin.com/in/santhosh-siva-385ab018b/">
+				<a href="https://www.linkedin.com/in/santhosh-siva-385ab018b/" target="_blank" rel="noopener noreferrer">
 					linkedin.com
 				</a>
-				<a href="https://github.com/grace-luminous/guitar-tab-maker-react">github.com</a>
+				<a href="https://github.com/grace-luminous/guitar-tab-maker-react" target="_blank" rel="noopener noreferrer">github.com</a>
 			</div>
 		</div>
 	);
